239393f2bd5f9653cbb524931b7d47581c5b9e03
aevsai <a.evsikov.ru@gmail.com>
2025-04-05 22:28:30 +0300

    fix uv

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'Installing Python dependencies for OCR...'
-echo 'Installing uv'
-curl -LsSf https://astral.sh/uv/install.sh | sh
-echo 'Using uv to install markitdown'
-uv sync
-echo 'Finished install Python dependencies'
